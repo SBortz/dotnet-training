4ac45dcfd26e1dd530edd01ab31b8c1a361a4986
Sebastian Bortz <sebastianbortz@gmail.com>
2025-12-05 09:47:25 +0100

    Added co- and contravariance tests, switched to .NET 10

---
+++ 04-SOLID/03-LiskovSubstitutionPattern.Tests/ContravarianceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace _03_LiskovSubstitutionPattern.Tests;
+public class WeaponSetter : IContravariant<Weapon>
+{
+    private Weapon? _weapon;
+    public void Set(Weapon value)
+        => _weapon = value;
+}
+
+public interface IContravariant<in T>
+{
+    void Set(T value);
+}
+
+public class ContravarianceTests
+{
+    [Test]
+    public void ContravarianceTest()
+    {
+        IContravariant<Weapon> weaponSetter = new WeaponSetter();
+        IContravariant<Sword> swordSetter = weaponSetter;
+        Assert.That(swordSetter, Is.SameAs(weaponSetter));
+        // Contravariance: Weapon > Sword > TwoHandedSword
+        weaponSetter.Set(new Weapon());
+        weaponSetter.Set(new Sword());
+        weaponSetter.Set(new TwoHandedSword());
+        // Contravariance: Sword > TwoHandedSword
+        swordSetter.Set(new Sword());
+        swordSetter.Set(new TwoHandedSword());
+    }
+}
---
+++ 04-SOLID/03-LiskovSubstitutionPattern.Tests/CovarianceTests.cs
@@ -0,0 +1,35 @@
+﻿namespace _03_LiskovSubstitutionPattern.Tests;
+
+public record class Weapon { }
+public record class Sword : Weapon { }
+
+public record class TwoHandedSword : Sword { }
+
+public interface ICovariant<out T>
+{
+    T Get();
+}
+
+public class SwordGetter : ICovariant<Sword>
+{
+    private static readonly Sword _instance = new();
+    public Sword Get() => _instance;
+}
+
+
+public class Tests
+{
+    [Test]
+    public void Covariance_Test()
+    {
+        ICovariant<Sword> swordGetter = new SwordGetter();
+        ICovariant<Weapon> weaponGetter = swordGetter;
+        Assert.That(swordGetter, Is.SameAs(weaponGetter));
+        Sword sword = swordGetter.Get();
+        Weapon weapon = weaponGetter.Get();
+        Assert.That(sword, Is.TypeOf<Sword>());
+        Assert.That(weapon, Is.TypeOf<Sword>());
+        Assert.That(sword, Is.Not.Null);
+        Assert.That(weapon, Is.Not.Null);
+    }
+}
