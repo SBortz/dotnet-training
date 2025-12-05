f9181810d8e9ee7cdcb478077785a36a3ba8555f
Sebastian Bortz <sebastianbortz@gmail.com>
2025-12-05 10:41:53 +0100

    added more SOLID projects

---
+++ 04-SOLID/04-InterfaceSegregationPrinciple/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace _04_InterfaceSegregationPrinciple;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ 04-SOLID/05-DependencyInversionPrinciple/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace _05_DependencyInversionPrinciple;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
